95a982425806d4c760653ab4230b10c675e6edea
DarioDeMaio <demaio.dario95@gmail.com>
2025-05-29 13:03:27 +0200

    feat: views, dashboard

--- xr2guide/xr2guide_core/admin.py
+++ xr2guide/xr2guide_core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Tour, Waypoint, WaypointView, MediaItem, WaypointViewImage
 
-# Register your models here.
+
+admin.site.register(Tour)
+admin.site.register(Waypoint)
+admin.site.register(WaypointView)
+admin.site.register(MediaItem)
+admin.site.register(WaypointViewImage)
