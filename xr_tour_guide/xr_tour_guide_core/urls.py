336dcf4c269ff67d1c346bf03e03b57acd796cd2
DarioDeMaio <demaio.dario95@gmail.com>
2025-06-05 16:34:24 +0200

    feat: personal info api, tour details

--- xr_tour_guide/xr_tour_guide_core/urls.py
+++ xr_tour_guide/xr_tour_guide_core/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 import nested_admin
-from views import tour_list
+from .views import tour_list, tour_details, profile_details, update_profile, delete_account, update_password
 
 urlpatterns = [
     path('tour_list/<str:category>/', tour_list, name='tour_list'),
+    path('tour_details/<int:pk>/', tour_details, name='tour_details'),
+    path('profile_details/', profile_details, name='profile_details'),
+    path('update_profile/', update_profile, name='update_profile'),
+    path('delete_account/', delete_account, name='delete_account'),
+    path('update_password/', update_password, name='update_password'),
 ]
--- xr_tour_guide/xr_tour_guide_core/views.py
+++ xr_tour_guide/xr_tour_guide_core/views.py
@@ -1,17 +1,83 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Tour
 from .serializers import TourSerializer
 from django.db.models import Q
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(['GET'])
 def tour_list(request, category):
     searchTerm = request.GET.get('searchTerm', '')
-    filters = Q(categoria__iexact=category)
+    filters = Q(category__iexact=category)
     if searchTerm:
         filters &=Q(nome__icontains=searchTerm) | Q(sottotitolo__icontains=searchTerm) | Q(descrizione__icontains=searchTerm) | Q(luogo__icontains=searchTerm) | Q(coordinate__icontains=searchTerm)
     tours = Tour.objects.filter(filters)
     serializer = TourSerializer(tours, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def tour_details(request, pk):
+    tour = Tour.objects.get(pk=pk)
+    serializer = TourSerializer(tour)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def profile_details(request):
+    user = request.user
+    serializer = UserSerializer(user)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def update_profile(request):
+    user = request.user
+    first_name = request.data.get('firstName', '').strip()
+    last_name = request.data.get('lastName', '').strip()
+    email = request.data.get('email', '').strip()
+
+    if first_name:
+        user.first_name = first_name
+    if last_name:
+        user.last_name = last_name
+    if email:
+        user.email = email
+
+    user.save()
+    return Response({"detail": "Profile updated successfully."}, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def update_password(request):
+    user = request.user
+    old_password = request.data.get('oldPassword')
+    new_password = request.data.get('newPassword')
+
+    if not user.check_password(old_password):
+        return Response({"detail": "Old password is incorrect."}, status=status.HTTP_400_BAD_REQUEST)
+
+    user.set_password(new_password)
+    user.save()
+    return Response({"detail": "Password updated successfully."}, status=status.HTTP_200_OK)
+
+# @api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+# def forget_password(request):
+#     user = request.user
+#     user.set_unusable_password()
+#     user.save()
+#     return Response({"detail": "Password removed. Reset necessary via email."}, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def delete_account(request):
+    user = request.user
+    password = request.data.get('password')
+
+    if not user.check_password(password):
+        return Response({"detail": "Password is incorrect."}, status=status.HTTP_400_BAD_REQUEST)
+
+    user.delete()
+    return Response({"detail": "Account deleted successfully."}, status=status.HTTP_200_OK)
