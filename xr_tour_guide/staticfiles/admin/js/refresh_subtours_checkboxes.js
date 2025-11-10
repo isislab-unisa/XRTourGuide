82448ebddbf717435e2d983f19868185a2d13632
DarioDeMaio <demaio.dario95@gmail.com>
2025-11-10 16:11:59 +0100

    fix: insert subtours

--- xr_tour_guide/staticfiles/admin/js/refresh_subtours_checkboxes.js
+++ xr_tour_guide/staticfiles/admin/js/refresh_subtours_checkboxes.js
@@ -1,55 +1,18 @@
-(function($) {
-    $(document).ready(function() {
-        // Function to refresh subtours checkboxes
-        function refreshSubtours() {
-            var tourId = $('#tour_form').data('tour-id') || null;
-            var currentUrl = window.location.pathname;
-            
-            // Store currently selected subtours
-            var selectedSubtours = [];
-            $('input[name="sub_tours"]:checked').each(function() {
-                selectedSubtours.push($(this).val());
-            });
-            
-            // Make AJAX request to get updated subtours
-            $.ajax({
-                url: currentUrl + 'get_subtours/',
-                type: 'GET',
-                data: {
-                    'tour_id': tourId
-                },
-                success: function(response) {
-                    // Update the subtours checkboxes
-                    var container = $('#id_sub_tours').parent();
-                    container.html(response.html);
-                    
-                    // Restore selected subtours
-                    selectedSubtours.forEach(function(id) {
-                        $('input[name="sub_tours"][value="' + id + '"]').prop('checked', true);
-                    });
-                },
-                error: function(xhr, status, error) {
-                    console.error('Error refreshing subtours:', error);
-                }
-            });
+(function() {
+    const originalDismiss = window.dismissAddRelatedObjectPopup;
+
+    window.dismissAddRelatedObjectPopup = function(win, newId, newRepr) {
+        originalDismiss(win, newId, newRepr);
+
+        const label = document.createElement('label');
+        label.innerHTML = `
+            <input type="checkbox" name="sub_tours" value="${newId}" checked>
+            ${newRepr}
+        `;
+
+        const subToursContainer = document.querySelector('div#id_sub_tours, div.field-sub_tours');
+        if (subToursContainer) {
+            subToursContainer.appendChild(label);
         }
-        
-        // Add refresh button next to subtours field
-        var subtourField = $('.field-sub_tours');
-        if (subtourField.length) {
-            var refreshBtn = $('<button type="button" class="button" style="margin-left: 10px;">Refresh Subtours</button>');
-            refreshBtn.on('click', function(e) {
-                e.preventDefault();
-                refreshSubtours();
-            });
-            subtourField.find('label').first().append(refreshBtn);
-        }
-        
-        // Optional: Auto-refresh when the page regains focus (user comes back from creating a subtour)
-        $(window).on('focus', function() {
-            if ($('.field-sub_tours').length) {
-                refreshSubtours();
-            }
-        });
-    });
-})(django.jQuery);
+    };
+})();
