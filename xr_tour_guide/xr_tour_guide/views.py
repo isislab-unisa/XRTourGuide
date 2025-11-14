6fab5d8ab78b251494818c178f7ff9bb05a1961d
DarioDeMaio <demaio.dario95@gmail.com>
2025-11-14 12:54:16 +0100

    fix: add user information in index.html

--- xr_tour_guide/xr_tour_guide/views.py
+++ xr_tour_guide/xr_tour_guide/views.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 from unfold.views import UnfoldModelAdminViewMixin
-from xr_tour_guide_core.models import Tour
+from xr_tour_guide_core.models import Tour, Review
+from django.db.models import Avg, Count
 
 
 admin.site.index_title = 'Dashboard'
@@ -13,7 +14,18 @@ class DashboardView(UnfoldModelAdminViewMixin, TemplateView):
     template_name = "admin/index.html"
     
 def dashboard_callback(request, context):
+    user_tours = Tour.objects.filter(parent_tours__isnull=True, is_subtour=False, user=request.user)
+    reviews = Review.objects.filter(user=request.user)
+
+    tour_stats = user_tours.annotate(
+        review_count=Count('reviews'),
+        avg_rating=Avg('reviews__rating'),
+        subtour_count=Count('sub_tours')
+    )
+
     context.update({
-        "tours": Tour.objects.filter(parent_tours__isnull=True, is_subtour=False, user=request.user),
+        "tours": user_tours,
+        "reviews": reviews,
+        "tour_stats": tour_stats,
     })
     return context
