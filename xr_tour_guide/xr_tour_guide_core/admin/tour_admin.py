7741fa26486862bae57cfb83fae8f11ffabd52fd
DarioDeMaio <demaio.dario95@gmail.com>
2026-01-05 17:20:13 +0100

    fix: status management

--- xr_tour_guide/xr_tour_guide_core/admin/tour_admin.py
+++ xr_tour_guide/xr_tour_guide_core/admin/tour_admin.py
@@ -17,10 +17,9 @@ class TourAdmin(nested_admin.NestedModelAdmin, ModelAdmin):
     show_facets = admin.ShowFacets.ALLOW
     hide_ordering_field = True
     compressed_fields = True
-    warn_unsaved_form = True
     
     list_display = ('title', 'place', 'category', 'status_badge', 'creation_time', 'user')
-    readonly_fields = ['user', 'creation_time', 'status_info']
+    readonly_fields = ['user', 'creation_time', 'status_info', 'status_badge', 'status']
     list_filter = ['category', 'status', 'place', 'creation_time']
     search_fields = ('title', 'subtitle', 'description', 'place')
     date_hierarchy = 'creation_time'
